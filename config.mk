0ee7e7e7179386d0cb1a359f3d9ce2603df51d68
Marko Man <darkobas@gmail.com>
2020-01-12 22:02:10 +0100

    EdithUI: Add black theme for dark mode [3/3]

--- config.mk
+++ config.mk
@@ -7,8 +7,11 @@ include $(EDITHUI_PATH)/bootanimation/bootanimation.mk
 PRODUCT_PACKAGES += \
 	WallpaperDefaultOverlay
 
+# Theme
+PRODUCT_PACKAGES += \
+    EdithBlackTheme
+
 # Power off charing resource
 PRODUCT_PACKAGES += \
     pixel_charger_animation \
     pixel_charger_animation_vendor
-
