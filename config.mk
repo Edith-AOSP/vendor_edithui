a560cdb925d301f5c09ff36eba1b170e15cd6279
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-07-26 12:32:59 +0700

    EdithUI: Import Pixel charger from halcyonui

--- config.mk
+++ config.mk
@@ -6,3 +6,9 @@ include $(EDITHUI_PATH)/bootanimation/bootanimation.mk
 # Overlays
 PRODUCT_PACKAGES += \
 	WallpaperDefaultOverlay
+
+# Power off charing resource
+PRODUCT_PACKAGES += \
+    pixel_charger_animation \
+    pixel_charger_animation_vendor
+
